ef06d2d93323ce92ce8aebe62efc9653e8737612
Peter Zingg <pzingg@kentfieldschools.org>
2015-05-26 10:25:31 -0700

    sent mail

--- notify.py
+++ notify.py
@@ -7,10 +7,10 @@ import smtplib
 if __name__ == "__main__":
   s = smtplib.SMTP('172.16.121.233', 25)
 
-  subject = 'List of files you own that are publicly shared'
+  subject = 'List of files you own that are shared publicly'
   fromEmail = 'pzingg@kentfieldschools.org'
 
-  for fname in glob('audit-*-2015*.txt'):
+  for fname in glob('audit-*.txt'):
       m = re.search('audit-(.+)-2015[\d]+\.txt', fname)
       doc = ''
       with open(fname) as f:
@@ -20,10 +20,19 @@ if __name__ == "__main__":
         user = m.group(1)
 
         lines = [ 'Dear %s,\n' % user, 
-          'Please review the list of files that you should review.',
+          'Please review the attached list of files that you have shared publicly.',
           'You can also download, print or add the attachment to your Google Drive.',
-          'Email me if you have any questions.  Thanks,\n',
-          'Peter\n\n' ]
+          '',
+          'And here\'s my cheat sheet with tips and reminders if you need some help:',
+          '',
+          'Making a Shared Google Drive Folder or Document Private',
+          'https://docs.google.com/a/kentfieldschools.org/document/d/1rzgGg1gfydocvX3XCkio2kEeOdgVK9syTB9F2JXK7Ok/edit?usp=sharing',
+          '',
+          'Email me if you have any questions.  Thanks,',
+          '',
+          'Peter',
+          '',
+          '' ]
 
         body = '\n'.join(lines) + doc
 
